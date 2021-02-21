0dfccb05928bacc082d2a834546b0d5445b1063e
djviodes <djviodes26@gmail.com>
2021-02-20 19:38:24 -0800

    Update 1 for API opening message

--- api/server.js
+++ api/server.js
@@ -12,16 +12,31 @@ const server = express();
 server.get("/", (req, res) => {
   res.send(`
     <h1>Postgres Game Picker API Documentation</h1>
-    <br />
     <ul>
         <li>Base URL: https://gamers-choice.herokuapp.com/</li>
     </ul>
     <br />
     <h3>Authentication:</h3>
-    <br />
     <ul>
         <li>Post - /auth/register</li>
         <li>Post - /auth/login</li>
+    </ul>
+    <br />
+    <h3>Users:</h3>
+    <ul>
+        <li>Get - /api/users - Gets all user profiles (Protected)</li>
+        <li>Get - /api/users/:id - Gets a specified user profile (Protected)</li>
+        <li>Put - /api/users/:id - Updates a specified user's profile (Protected)</li>
+        <li>Delete - /api/users/:id - Deletes a specified user's profile (Protected)</li>
+    </ul>
+    <br />
+    <h3>Video Games:</h3>
+    <ul>
+        <li>Get - /api/games - Gets all the games</li>
+        <li>Get - /api/games/:id - Gets a specified game</li>
+        <li>Post - /api/games - Creates a new game (Protected)</li>
+        <li>Put - /api/games/:id - Updates a specified game (Protected)</li>
+        <li>Delete - /api/games/:id - Deletes a specified game (Protected)</li>
     </ul>
   `);
 });
